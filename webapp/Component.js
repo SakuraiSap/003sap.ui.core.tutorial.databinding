b464477a3792228ad80acf16f51044ef9e203e7f
SakuraiSap <sakurai.exam@gmail.com>
2024-03-26 15:04:06 +0900

    Step 5: One-Way Data Binding In contrast to the two-way binding behavior shown above, one-way data binding is also possible. Here, data is transported in one direction only: from the model, through the binding instance to the consumer (usually the property of a control), but never in the other direction. In this example, we will change the previous example to use one-way data binding. This will illustrate how the flow of data from the user interface back to the model can be switched off if required.

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,12 +1,17 @@
 sap.ui.define([
-	"sap/ui/core/UIComponent"
-], (UIComponent) => {
+	"sap/ui/core/UIComponent",
+	"sap/ui/model/BindingMode"
+], (UIComponent, BindingMode) => {
 	"use strict";
 
 	return UIComponent.extend("ui5.databinding.Component", {
 		metadata: {
 			interfaces: ["sap.ui.core.IAsyncContentCreation"],
 			manifest: "json"
+		},
+		init(){
+			UIComponent.prototype.init.apply(this, arguments);
+			this.getModel().setDefaultBindingMode(BindingMode.OneWay);
 		}
 	});
 });
